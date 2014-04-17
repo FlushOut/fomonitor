18776ac1e99e9422b54a2f22be19bc24b79c6a5a
Manuel Moyano <mnlmoyano@gmail.com>
2014-04-16 21:07:44 -0300

    update bd

---
+++ web/pages/points.php
@@ -0,0 +1,284 @@
+<?php
+require_once("../config.php");
+
+$category = new category();
+$list_categories = $category->list_categories(1);
+
+
+if ($_POST['action'] == 'Save') {
+         $category->save(1, $_POST['txtDescription']);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Tables - Stilearn Admin Bootstrap</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="" />
+        <meta name="author" content="stilearning" />
+
+        <!-- google font -->
+        <link href="http://fonts.googleapis.com/css?family=Aclonica:regular" rel="stylesheet" type="text/css" />
+
+        <!-- styles -->
+        <link href="../css/bootstrap.css" rel="stylesheet" />
+        <link href="../css/bootstrap-responsive.css" rel="stylesheet" />
+        <link href="../css/stilearn.css" rel="stylesheet" />
+        <link href="../css/stilearn-responsive.css" rel="stylesheet" />
+        <link href="../css/stilearn-helper.css" rel="stylesheet" />
+        <link href="../css/stilearn-icon.css" rel="stylesheet" />
+        <link href="../css/font-awesome.css" rel="stylesheet" />
+        <link href="../css/animate.css" rel="stylesheet" />
+        <link href="../css/uniform.default.css" rel="stylesheet" />
+        
+        <link href="../css/DT_bootstrap.css" rel="stylesheet" />
+        <link href="../css/responsive-tables.css" rel="stylesheet" />
+        
+        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+
+    <body>
+        <!-- start header -->
+        <?php include("../includes/_header.php"); ?>
+        <!-- end header -->
+
+
+        <!-- section content -->
+        <section class="section">
+            <div class="row-fluid">
+
+                <!-- start left_menu -->
+                <?php include("../includes/_left_menu.php"); ?>
+                <!-- end left_menu -->
+                
+                <!-- span content -->
+                <div class="span9">
+                    <!-- content -->
+                    <div class="content">
+                        <!-- content-header -->
+                        <div class="content-header">
+                            <ul class="content-header-action pull-right">
+                                <li>
+                                    <a href="#">
+                                        <div class="badge-circle grd-green color-white"><i class="icofont-plus-sign"></i></div>
+                                        <div class="action-text color-green">8765 <span class="helper-font-small color-silver-dark">Visits</span></div>
+                                    </a>
+                                </li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a href="#">
+                                        <div class="badge-circle grd-teal color-white"><i class="icofont-user-md"></i></div>
+                                        <div class="action-text color-teal">1437 <span class="helper-font-small color-silver-dark">Users</span></div>
+                                    </a>
+                                </li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a href="#">
+                                        <div class="badge-circle grd-orange color-white">$</div>
+                                        <div class="action-text color-orange">4367 <span class="helper-font-small color-silver-dark">Orders</span></div>
+                                    </a>
+                                </li>
+                            </ul>
+                            <h2><i class="icofont-table"></i> Points</h2>
+                        </div><!-- /content-header -->
+                        
+                        <!-- content-breadcrumb -->
+                        <div class="content-breadcrumb">
+                            <!--breadcrumb-nav-->
+                            <ul class="breadcrumb-nav pull-right">
+                                <li class="divider"></li>
+                                <li class="btn-group">
+                                    <a href="#" class="btn btn-small btn-link">
+                                        <i class="icofont-money"></i> Orders <span class="color-red">(+12)</span>
+                                    </a>
+                                </li>
+                                <li class="divider"></li>
+                                <li class="btn-group">
+                                    <a href="#" class="btn btn-small btn-link">
+                                        <i class="icofont-user"></i> Users <span class="color-red">(+34)</span>
+                                    </a>
+                                </li>
+                                <li class="divider"></li>
+                                <li class="btn-group">
+                                    <a href="#" class="btn btn-small btn-link dropdown-toggle" data-toggle="dropdown">
+                                        <i class="icofont-tasks"></i> Tasks
+                                        <i class="icofont-caret-down"></i>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Some Action</a></li>
+                                        <li><a href="#">Other Action</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Something Else</a></li>
+                                    </ul>
+                                </li>
+                            </ul><!--/breadcrumb-nav-->
+                            
+                            <!--breadcrumb-->
+                            <ul class="breadcrumb">
+                                <li><a href="index.html"><i class="icofont-home"></i> Points</a> <span class="divider">&rsaquo;</span></li>
+                                <li class="active">List</li>
+                            </ul><!--/breadcrumb-->
+                        </div><!-- /content-breadcrumb -->
+                        
+                        <!-- content-body -->
+                        <div class="content-body">
+                            <!-- tables -->
+                            <!--datatables-->
+                            <div class="row-fluid">
+                                <div class="span12">
+                                    <div class="box corner-all">
+                                        <div class="box-header grd-white corner-top">
+                                            <div class="header-control">
+                                                <a data-box="collapse"><i class="icofont-caret-up"></i></a>
+                                                <a data-box="close" data-hide="bounceOutRight">&times;</a>
+                                            </div>
+                                            <span>Points&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                                            <a href="#myModal" role="button" class="btn" data-toggle="modal" id="aAdd">Add</a>
+                                                
+                                                    <!-- Modal -->
+                                                    <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                                        <div class="modal-header">
+                                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                                            <h3 id="myModalLabel">Information</h3>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <form class="form-horizontal" id="form-validate" action="" method="post" />
+                                                                    <tr>
+                                                                        <td>Description</td>
+                                                                        <td><input type="text" id="txtDescription" name="txtDescription" class="grd-white" data-validate="{required: true, messages:{required:'Please enter field required'}}" name="required" id="required" /></td>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td><button class="btn" data-dismiss="modal" aria-hidden="true">Close</button></td>
+                                                                        <td><button class="btn btn-primary" id="btnSave" name="action" value="Save">Save</button></td>
+                                                                    </tr>
+                                                            </form>
+                                                        </div>
+                                                        
+                                                    </div>
+                                        </div>
+                                        <div class="box-body">
+                                            <table id="datatables" class="table table-bordered table-striped responsive">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Id</th>
+                                                        <th>Description</th>
+                                                        <th>Satus</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <?php foreach ($list_categories as $item) { ?>
+                                                        <tr>
+                                                            <td><?php echo $item->id; ?></td>
+                                                            <td><?php echo $item->description; ?></td>
+                                                            <td>
+                                                                <button type="button" class="btn btn-link" onclick="location.href='/pages/edit_point.php?id=<?php echo $item->id ?>'">Edit</button>
+                                                                <button type="button" class="btn btn-link" onclick="location.href='#">Delete</button>
+                                                            </td>
+                                                        </tr>
+                                                    <?php } ?>
+                                                </tbody>
+                                            </table>
+                                            
+                                        </div><!-- /box-body -->
+                                    </div><!-- /box -->
+                                </div><!-- /span -->
+                            </div><!--/datatables-->
+                            
+                            
+                            <!--/tables-->
+                        </div><!--/content-body -->
+                    </div><!-- /content -->
+                </div><!-- /span content -->
+                
+                <!-- start right_menu -->
+                <?php include("../includes/_right_menu.php"); ?>
+                <!-- end right_menu -->
+
+            </div>
+        </section>
+
+        <!-- start left_menu -->
+        <?php include("../includes/_footer.php"); ?>
+        <!-- end left_menu -->
+
+
+        <!-- javascript
+        ================================================== -->
+        <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+        <script src="../js/jquery.js"></script>
+        <script src="../js/bootstrap.js"></script>
+        <script src="../js/uniform/jquery.uniform.js"></script>
+
+        <script src="../js/validate/jquery.validate.js"></script>
+        <script src="../js/validate/jquery.metadata.js"></script>
+        
+        <script src="../js/datatables/jquery.dataTables.min.js"></script>
+        <script src="../js/datatables/extras/ZeroClipboard.js"></script>
+        <script src="../js/datatables/extras/TableTools.min.js"></script>
+        <script src="../js/datatables/DT_bootstrap.js"></script>
+        <script src="../js/responsive-tables/responsive-tables.js"></script>
+        
+        <!-- required stilearn template js, for full feature-->
+        <script src="../js/holder.js"></script>
+        <script src="../js/stilearn-base.js"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function() {
+                // try your js
+
+                // validate form
+                $("a#aAdd").bind('click', function () {
+                    $("#txtDescription").val('');
+                    var validator = $( "#form-validate" ).validate();
+                    validator.resetForm();
+                    
+                });
+
+                $('#form-validate').validate();
+                
+                // uniform
+                $('[data-form=uniform]').uniform();
+                
+                // datatables
+                $('#datatables').dataTable( {
+                    "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+                    "sPaginationType": "bootstrap",
+                    "oLanguage": {
+                            "sLengthMenu": "_MENU_ records per page"
+                    }
+                });
+                
+                // datatables table tools
+                $('#datatablestools').dataTable({
+                    "sDom": "<'row-fluid'<'span6'T><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+                    "oTableTools": {
+                        "aButtons": [
+                            "copy",
+                            "print",
+                            {
+                                "sExtends":    "collection",
+                                "sButtonText": 'Save <span class="caret" />',
+                                "aButtons":    [ 
+                                    "xls", 
+                                    "csv",
+                                    {
+                                        "sExtends": "pdf",
+                                        "sPdfOrientation": "landscape",
+                                        "sPdfMessage": "Your custom message would go here."
+                                    }
+                                ]
+                            }
+                        ],
+                        "sSwfPath": "../js/datatables/swf/copy_csv_xls_pdf.swf"
+                    }
+                });
+            });
+      
+        </script>
+    </body>
+</html>
