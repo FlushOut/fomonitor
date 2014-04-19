b7b052deac80920a138facc562c817bfeb956d9d
JohnEduardo <jebmarca@gmail.com>
2014-04-19 01:37:35 -0300

    closed #3
    Resolved > Error to open google map of ponits modal to insert new row

--- web/pages/points.php
+++ web/pages/points.php
@@ -19,7 +19,7 @@ if ($_POST['action'] == 'Save') {
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml"> 
     <head>
         <meta charset="utf-8" />
         <title>Tables - Stilearn Admin Bootstrap</title>
