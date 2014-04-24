fc4345b3d348f89a1b231214d4ba3a4cfb1eb14c
Manuel Moyano <mnlmoyano@gmail.com>
2014-04-24 14:39:51 -0300

    #15 interface of pages/map.php, funcionality yet

--- web/includes/_header.php
+++ web/includes/_header.php
@@ -33,15 +33,6 @@ if ($_POST['action'] == 'SaveCompany') {
                     <!--/panel name-->
 
                     <div class="span6">
-                        <!--panel search-->
-                        <div class="panel-search">
-                            <form />
-                                <div class="input-icon-append">
-                                    <button type="submit" rel="tooltip-bottom" title="search" class="icon"><i class="icofont-search"></i></button>
-                                    <input class="input-large search-query grd-white" maxlength="23" placeholder="Search here..." type="text" />
-                                </div>
-                            </form>
-                        </div><!--/panel search-->
                     </div>
                     <div class="span4">
                         <!--panel button ext-->
---
+++ web/pages/map.php
@@ -0,0 +1,285 @@
+<?php
+require_once("../config.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Tables - Stilearn Admin Bootstrap</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="" />
+        <meta name="author" content="stilearning" />
+
+        <!-- google font -->
+        <link href="http://fonts.googleapis.com/css?family=Aclonica:regular" rel="stylesheet" type="text/css" />
+
+        <!-- styles -->
+        <link href="../css/bootstrap.css" rel="stylesheet" />
+        <link href="../css/bootstrap-responsive.css" rel="stylesheet" />
+        <link href="../css/stilearn.css" rel="stylesheet" />
+        <link href="../css/stilearn-responsive.css" rel="stylesheet" />
+        <link href="../css/stilearn-helper.css" rel="stylesheet" />
+        <link href="../css/stilearn-icon.css" rel="stylesheet" />
+        <link href="../css/font-awesome.css" rel="stylesheet" />
+        <link href="../css/animate.css" rel="stylesheet" />
+        <link href="../css/uniform.default.css" rel="stylesheet" />
+
+        <link href="../css/select2.css" rel="stylesheet" />
+        
+        <link href="../css/DT_bootstrap.css" rel="stylesheet" />
+        <link href="../css/responsive-tables.css" rel="stylesheet" />
+        
+        <script src="../js/jquery.js"></script>
+        <script src="../js/jquery-ui.min.js"></script> <!-- this for sliders-->        
+        
+        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+
+    <body>
+        <!-- start header -->
+        <?php include("../includes/_header.php"); ?>
+        <!-- end header -->
+
+
+        <!-- section content -->
+        <section class="section">
+            <div class="row-fluid">
+
+                <!-- start left_menu -->
+                <?php include("../includes/_left_menu.php"); ?>
+                <!-- end left_menu -->
+                
+                <!-- span content -->
+                <div class="span9">
+                    <!-- content -->
+                    <div class="content">
+                        <!-- content-header -->
+                        <div class="content-header">
+                            <ul class="content-header-action pull-right">
+                                <li>
+                                    <a href="#">
+                                        <div class="badge-circle grd-green color-white"><i class="icofont-plus-sign"></i></div>
+                                        <div class="action-text color-green">8765 <span class="helper-font-small color-silver-dark">Visits</span></div>
+                                    </a>
+                                </li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a href="#">
+                                        <div class="badge-circle grd-teal color-white"><i class="icofont-user-md"></i></div>
+                                        <div class="action-text color-teal">1437 <span class="helper-font-small color-silver-dark">Users</span></div>
+                                    </a>
+                                </li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a href="#">
+                                        <div class="badge-circle grd-orange color-white">$</div>
+                                        <div class="action-text color-orange">4367 <span class="helper-font-small color-silver-dark">Orders</span></div>
+                                    </a>
+                                </li>
+                            </ul>
+                            <h2><i class="icofont-table"></i> Map</h2>
+                        </div><!-- /content-header -->
+                        
+                        <!-- content-breadcrumb -->
+                        <div class="content-breadcrumb">
+                            <!--breadcrumb-nav-->
+                            <ul class="breadcrumb-nav pull-right">
+                                <li class="divider"></li>
+                                <li class="btn-group">
+                                    <a href="#" class="btn btn-small btn-link">
+                                        <i class="icofont-money"></i> Orders <span class="color-red">(+12)</span>
+                                    </a>
+                                </li>
+                                <li class="divider"></li>
+                                <li class="btn-group">
+                                    <a href="#" class="btn btn-small btn-link">
+                                        <i class="icofont-user"></i> Users <span class="color-red">(+34)</span>
+                                    </a>
+                                </li>
+                                <li class="divider"></li>
+                                <li class="btn-group">
+                                    <a href="#" class="btn btn-small btn-link dropdown-toggle" data-toggle="dropdown">
+                                        <i class="icofont-tasks"></i> Tasks
+                                        <i class="icofont-caret-down"></i>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Some Action</a></li>
+                                        <li><a href="#">Other Action</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Something Else</a></li>
+                                    </ul>
+                                </li>
+                            </ul><!--/breadcrumb-nav-->
+                            
+                            <!--breadcrumb-->
+                            <ul class="breadcrumb">
+                                <li><a href="index.html"><i class="icofont-home"></i> Map</a> <span class="divider">&rsaquo;</span></li>
+                                <li class="active">Map</li>
+                            </ul><!--/breadcrumb-->
+                        </div><!-- /content-breadcrumb -->
+                        
+                        <!-- content-body -->
+                        <div class="content-body">
+                            <!-- tables -->
+                            <!--datatables-->
+                            <div class="row-fluid">
+                                <div class="span12">
+                                    <div class="box corner-all">
+                                        <div class="box-header grd-white corner-top">
+                                            <div class="header-control">
+                                                <a data-box="collapse"><i class="icofont-caret-up"></i></a>
+                                                <a data-box="close" data-hide="bounceOutRight">&times;</a>
+                                            </div>
+                                            <span>Map</span>
+                                            
+                                        </div>
+                                        <div class="box-body">
+                                           <div class="control-group">
+                                                <div class="controls">
+                                                    <label class="checkbox inline">
+                                                        <input type="checkbox" data-form="uniform" name="inputCheckbox" id="inlineCheckbox1" value="option1" /> All Users
+                                                    </label>
+                                                    &nbsp;&nbsp;&nbsp;&nbsp;
+                                                    <select id="inputSelectMulti" data-form="select2" style="width:200px" data-placeholder="Select users..." multiple="">
+                                                        <option />User 1
+                                                        <option />User 2
+                                                        <option />User 3
+                                                        <option />User 4
+                                                        <option />User 5
+                                                        <option />User 6
+                                                        <option />User 7
+                                                        <option />User 8
+                                                    </select>
+                                                    &nbsp;&nbsp;
+                                                    <button type="button" class="btn">Clear</button>
+                                                    <label class="checkbox inline">
+                                                        <input type="checkbox" data-form="uniform" name="inputCheckbox2" id="inlineCheckbox2" value="option2" /> All Points
+                                                    </label>
+                                                    &nbsp;&nbsp;&nbsp;&nbsp;
+                                                    <select id="inputSelectMulti2" data-form="select2" style="width:200px" data-placeholder="Select points..." multiple="">
+                                                        <option />Point 1
+                                                        <option />Point 2
+                                                        <option />Point 3
+                                                        <option />Point 4
+                                                        <option />Point 5
+                                                        <option />Point 6
+                                                        <option />Point 7
+                                                        <option />Point 8
+                                                    </select>
+                                                    &nbsp;&nbsp;
+                                                    <button type="button" class="btn">Clear</button>
+                                                </div>
+                                            </div>
+                                            <div class="mapa" id="mapa" style="height:450px; width: 100%;"></div>
+                                        </div><!-- /box-body -->
+                                    </div><!-- /box -->
+                                </div><!-- /span -->
+                            </div><!--/datatables-->
+                            <!--/tables-->
+                        </div><!--/content-body -->
+                    </div><!-- /content -->
+                </div><!-- /span content -->
+                
+                <!-- start right_menu -->
+                <?php include("../includes/_right_menu.php"); ?>
+                <!-- end right_menu -->
+
+            </div>
+        </section>
+
+        <!-- start left_menu -->
+        <?php include("../includes/_footer.php"); ?>
+        <!-- end left_menu -->
+
+
+        <!-- javascript
+        ================================================== -->
+        <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+        <script src="../js/bootstrap.js"></script>
+        <script src="../js/uniform/jquery.uniform.js"></script>
+
+        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+
+        <script src="../js/select2/select2.js"></script>
+
+        <script src="../js/validate/jquery.validate.js"></script>
+        <script src="../js/validate/jquery.metadata.js"></script>
+        
+        <script src="../js/datatables/jquery.dataTables.min.js"></script>
+        <script src="../js/datatables/extras/ZeroClipboard.js"></script>
+        <script src="../js/datatables/extras/TableTools.min.js"></script>
+        <script src="../js/datatables/DT_bootstrap.js"></script>
+        <script src="../js/responsive-tables/responsive-tables.js"></script>
+        
+        <!-- required stilearn template js, for full feature-->
+        <script src="../js/holder.js"></script>
+        <script src="../js/stilearn-base.js"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function() {
+
+                var myLatlng;
+                var mapOptions;
+                var map;
+
+                myLatlng = new google.maps.LatLng('-23.565262', '-46.683653');
+                        mapOptions = {
+                            zoom: 8,
+                            center: myLatlng
+                        };
+
+                map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
+
+                // try your js
+
+                $('#form-validate').validate();
+                
+                // uniform
+                $('[data-form=uniform]').uniform();
+
+                // select2
+                $('[data-form=select2]').select2();
+                
+                // datatables
+                $('#datatables').dataTable( {
+                    "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+                    "sPaginationType": "bootstrap",
+                    "oLanguage": {
+                            "sLengthMenu": "_MENU_ records per page"
+                    }
+                });
+                
+                // datatables table tools
+                $('#datatablestools').dataTable({
+                    "sDom": "<'row-fluid'<'span6'T><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
+                    "oTableTools": {
+                        "aButtons": [
+                            "copy",
+                            "print",
+                            {
+                                "sExtends":    "collection",
+                                "sButtonText": 'Save <span class="caret" />',
+                                "aButtons":    [ 
+                                    "xls", 
+                                    "csv",
+                                    {
+                                        "sExtends": "pdf",
+                                        "sPdfOrientation": "landscape",
+                                        "sPdfMessage": "Your custom message would go here."
+                                    }
+                                ]
+                            }
+                        ],
+                        "sSwfPath": "../js/datatables/swf/copy_csv_xls_pdf.swf"
+                    }
+                });
+            });
+      
+        </script>
+    </body>
+</html>
